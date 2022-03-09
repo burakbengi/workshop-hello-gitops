b324e1914af3da90ba4354bba567271f5989e9a0
Vecdi-Burak Bengi <burak.bengi@rwth-aachen.de>
2022-03-09 08:12:37 +0000

    changed yet another word

--- cmd/main_test.go
+++ cmd/main_test.go
@@ -21,7 +21,7 @@ func Test_handler(t *testing.T) {
 	}{
 		{
 			"status OK",
-			args{"/", http.StatusOK, fmt.Sprintf("Hello from new argocd deployed %s", host)},
+			args{"/", http.StatusOK, fmt.Sprintf("Hello from a different project %s", host)},
 		},
 	}
 
